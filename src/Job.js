5fd2d29da7184a160938996fea56bf2962f4777e
Stephen Wilson <stephen.ben.wilson@gmail.com>
2019-12-10 15:37:47 -0500

    Created Job(s) component structure

---
+++ src/Job.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function Job() 
+{
+  return(
+  <div className="Job">
+
+  </div>
+  )
+}
+
+export default Job;
---
+++ src/Jobs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Job from './Job';
+
+function Jobs(Job)
+{
+  return(
+  <div className="Jobs">
+    <Typography variant="h1">
+      Junior Level Software Jobs
+    </Typography>
+  </div>
+  )
+}
+
+export default Jobs;
