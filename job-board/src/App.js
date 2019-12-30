23baa20d4f8357fcc94c287c245a7ae6a9d4bdcd
Stephen Wilson <stephen.ben.wilson@gmail.com>
2019-12-30 13:50:26 -0500

    Added API to populate junior level github jobs into app data

--- job-board/src/App.js
+++ job-board/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Jobs from './Jobs';
 
+const JOB_API_URL = 'http://localhost:3001/jobs'
+
 const mockJobs = [
   {
     title: 'SWE 1',
@@ -21,10 +23,24 @@ const mockJobs = [
   }
 ]
 
+async function fetchJobs() {
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+
+  console.log({json});
+}
+
 function App() {
+
+  const [jobList, updateJobs] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchJobs();
+  }, [])
+
   return (
     <div className="App">
-      <Jobs jobs={mockJobs}/>
+      <Jobs jobs={jobList}/>
     </div>
   );
 }
--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,4 +1,11 @@
+
+
 var fetch = require('node-fetch');
+var redis = require("redis"), client = redis.createClient();
+
+const {promisify} = require('util');
+
+const setAsync = promisify(client.set).bind(client);
 
 const baseURL = 'https://jobs.github.com/positions.json';
 
@@ -6,6 +13,8 @@ async function fetchGithub()
 {
   let resultCount = 1, onPage = 0;
   const jobsList = [];
+
+  //fetch all pages
 
   while(resultCount > 0){
     const response = await fetch(`${baseURL}?page=${onPage}`);
@@ -15,8 +24,29 @@ async function fetchGithub()
     console.log('got ' + jobs.length + ' jobs');
     onPage++;
   }
-  
   console.log('got ' + jobsList.length + ' jobs in total');
+  //filter steps
+  const jrJobs = jobsList.filter(job => {
+    const jobTitle = job.title.toLowerCase();
+    let isJunior = true;
+    let filterKeywords = ['senior','manager','sr.','architect']
+
+    if(filterKeywords.some(substring => jobTitle.includes(substring))){
+      isJunior = false;
+    }
+
+    return isJunior;
+  })
+
+  console.log('filtered down to ' + jrJobs.length + ' jobs');
+  
+  
+
+  //set in redis
+  const success = await setAsync('github', JSON.stringify(jrJobs));
+
+  console.log({success});
+
 }
 
 fetchGithub();
